6d34690fc56ce200e23d7b4ba59b50b947edc220
Björn-Egil Dahlberg <psyeugenic@gmail.com>
2021-01-09 11:17:09 +0100

    Utilization

--- src/monarch_lib.erl
+++ src/monarch_lib.erl
@@ -11,7 +11,7 @@
 	 memory/0,
 	 disks/0,
 	 loadavg/0,
-	 cpu_util/0,
+	 util/0,
 	 processes/0]).
 
 -on_load(load_nif/0).
@@ -25,6 +25,15 @@ processes() -> erlang:nif_error(undef).
 disks()     -> erlang:nif_error(undef).
 cpu_util()  -> erlang:nif_error(undef).
 
+util() ->
+    %#{cpu_id => 7,idle => 13461918,kernel => 214612,nice_user => 0,user => 357013}
+    [begin
+         Tot = Idle + Kernel + Nice + User,
+         U#{idle := Idle/Tot,
+            kernel := Kernel/Tot,
+            nice_user := Nice/Tot,
+            user := User/Tot}
+     end || #{idle := Idle, kernel := Kernel, nice_user := Nice, user := User} = U <- cpu_util()].
 
 %% NIF handler
 
